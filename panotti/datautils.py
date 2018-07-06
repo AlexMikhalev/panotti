00dd3cfa1575d77b1636afc2a8fd2cfd3441b9ed
Alex Mikhalev <alex.mikhalev@gmail.com>
2018-07-06 19:08:27 +0100

    last

--- panotti/datautils.py
+++ panotti/datautils.py
@@ -151,8 +151,7 @@ def make_melgram(mono_sig, sr, n_mels=128):   # @keunwoochoi upgraded form 96 to
     #melgram = librosa.logamplitude(librosa.feature.melspectrogram(mono_sig,  # latest librosa deprecated logamplitude in favor of amplitude_to_db
     #    sr=sr, n_mels=96),ref_power=1.0)[np.newaxis,np.newaxis,:,:]
 
-    melgram = librosa.amplitude_to_db(librosa.feature.melspectrogram(mono_sig,
-        sr=sr, n_mels=n_mels)).[np.newaxis,:,:,np.newaxis]     # for TF we need to melgram
+    melgram = librosa.amplitude_to_db(librosa.feature.melspectrogram(mono_sig, sr=sr, n_mels=n_mels))[np.newaxis,:,:,np.newaxis]    # for TF we need to melgram
 
     '''
     # librosa docs also include a perceptual CQT example:
